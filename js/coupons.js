d0ee8f4d8de77c66ba33e0858ccf1c3647c007f3
yunbin.wangyb <yunbin.wangyb@alibaba-inc.com>
2016-01-30 16:21:43 +0800

    测试循环

--- js/coupons.js
+++ js/coupons.js
@@ -1,6 +1,6 @@
 $(function () {
-    datas.forEach(function(data) {
-        console.log(data['img'])
+    for (var i=0 ; i<datas.length ; i++){
+        var data = datas[i];
         var ele = $('\
             <li><div id="coupon">\
                     <div class="mod-a mod-a-good">\
@@ -10,7 +10,7 @@ $(function () {
                     <div class="mod-a-ln-bg"></div>\
             </div>\
             <div class="mod-a-icon"></div>\
-            <div class="mod-a-tit">' + data['name'] + '</div>\
+            <div class="mod-a-tit">' + data['name'] + i + '</div>\
             <div class="mod-a-sub-tit">' + data['valid'] + '</div>\
             <div class="mod-a-val"><span class="price"><span>￥</span>' + data['discount'] + '</span></div>\
             <div class="mod-a-tip">' + data['condition'] + '</div>\
@@ -19,5 +19,5 @@ $(function () {
             <div style="margin:0 8px;"><img src="' + data['img']+ '" style="width:100%;"/></div>\
             </li>')
         $('.coupons-container ul').append(ele);
-    });
+    };
 });
