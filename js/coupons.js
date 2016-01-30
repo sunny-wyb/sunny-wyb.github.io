748ec8123f8aa293ef77055158cf3ed98ded0076
yunbin.wangyb <yunbin.wangyb@alibaba-inc.com>
2016-01-30 16:16:07 +0800

    完成js

---
+++ js/coupons.js
@@ -0,0 +1,23 @@
+$(function () {
+    datas.forEach(function(data) {
+        console.log(data['img'])
+        var ele = $('\
+            <li><div id="coupon">\
+                    <div class="mod-a mod-a-good">\
+                    <div class="mod-a-h"></div>\
+            <div class="mod-a-f"></div>\
+            <div class="mod-a-ln">\
+                    <div class="mod-a-ln-bg"></div>\
+            </div>\
+            <div class="mod-a-icon"></div>\
+            <div class="mod-a-tit">' + data['name'] + '</div>\
+            <div class="mod-a-sub-tit">' + data['valid'] + '</div>\
+            <div class="mod-a-val"><span class="price"><span>￥</span>' + data['discount'] + '</span></div>\
+            <div class="mod-a-tip">' + data['condition'] + '</div>\
+            </div>\
+            </div>\
+            <div style="margin:0 8px;"><img src="' + data['img']+ '" style="width:100%;"/></div>\
+            </li>')
+        $('.coupons-container ul').append(ele);
+    });
+});
---
+++ js/data.js
@@ -0,0 +1,4 @@
+/**
+ * Created by admin on 16/1/30.
+ */
+var datas = [{'name':'foot宝宝商品优惠券','valid':'2016.01.26前有效','discount':'3','start_fee':'满 60 使用','img':'https://img.alicdn.com/imgextra/i4/1789418796/TB20Kt4hFXXXXbKXpXXXXXXXXXX_!!1789418796.jpg'}];
