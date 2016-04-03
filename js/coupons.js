ae00fde9a21314305bff876b7cecbad0bc9585c6
yunbin.wangyb <yunbin.wangyb@alibaba-inc.com>
2016-04-03 10:34:49 +0800

    提交

--- js/coupons.js
+++ js/coupons.js
@@ -2,7 +2,7 @@ $(function () {
     for (var i = 0; i < datas.length; i++) {
         var data = datas[i];
         var ele = $('\
-            <li><a href = "http://www.baidu.com"><div id="coupon">\
+            <li><a href = "taobaobridge.html?type=coupon&sellerId=1789418796&activityId=23b1031d13474c37b6c712601b32cf3d"><div id="coupon">\
                     <div class="mod-a mod-a-good">\
                     <div class="mod-a-h"></div>\
             <div class="mod-a-f"></div>\
@@ -16,7 +16,6 @@ $(function () {
             <div class="mod-a-tip">' + data['condition'] + '</div>\
             </div>\
             </div></a>\
-            <div style="margin:0 8px;"><a href="taobaobridge.html?type=offer&offer_id=523924009712"><img src="' + data['img'] + '" style="width:100%;"/></div>\
             </li>')
         $('.coupons-container ul').append(ele);
     }
