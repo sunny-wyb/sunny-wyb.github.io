86120645e8c75d87f378e0ac9cc6a9c7a68748fb
yunbin.wangyb <yunbin.wangyb@alibaba-inc.com>
2016-04-03 12:12:26 +0800

    提交

--- js/data.js
+++ js/data.js
@@ -2,5 +2,5 @@
  * Created by admin on 16/1/30.
  */
 var datas = [
-    {'name':'Foot宝宝优惠券1','valid':'2016.04.17前有效','discount':'10','condition':'满 50 使用','img':'https://img.alicdn.com/imgextra/i4/1789418796/TB20Kt4hFXXXXbKXpXXXXXXXXXX_!!1789418796.jpg' , 'seller_id':'1789418796' , 'activity_id':'2dd1ec9c8ee14264bfccc867eb8612b2'}
+    {'name':'Foot宝宝优惠券1','valid':'2016.04.17前有效','discount':'10','condition':'满 50 使用','img':'https://img.alicdn.com/imgextra/i4/1789418796/TB20Kt4hFXXXXbKXpXXXXXXXXXX_!!1789418796.jpg' , 'seller_id':'1789418796' , 'activity_id':'b148deed0cb043199cd1b9b33b5d5049'}
 ];
