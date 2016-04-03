de9f157685181ba518c14c219044c94fc929bbed
yunbin.wangyb <yunbin.wangyb@alibaba-inc.com>
2016-04-03 11:42:47 +0800

    提交

--- js/coupons.js
+++ js/coupons.js
@@ -2,7 +2,7 @@ $(function () {
     for (var i = 0; i < datas.length; i++) {
         var data = datas[i];
         var ele = $('\
-            <li><a href = "taobaobridge.html?type=coupon&seller_id=1789418796&activity_id=23b1031d13474c37b6c712601b32cf3d"><div id="coupon">\
+            <li><a href = "taobaobridge.html?type=coupon&seller_id=' + data['seller_id'] + '&activity_id=' + data['activity_id'] + '"><div id="coupon">\
                     <div class="mod-a mod-a-good">\
                     <div class="mod-a-h"></div>\
             <div class="mod-a-f"></div>\
--- js/data.js
+++ js/data.js
@@ -2,5 +2,5 @@
  * Created by admin on 16/1/30.
  */
 var datas = [
-    {'name':'foot宝宝商品优惠券','valid':'2016.01.26前有效','discount':'3','condition':'满 60 使用','img':'https://img.alicdn.com/imgextra/i4/1789418796/TB20Kt4hFXXXXbKXpXXXXXXXXXX_!!1789418796.jpg'}
+    {'name':'foot宝宝商品优惠券','valid':'2016.01.26前有效','discount':'3','condition':'满 60 使用','img':'https://img.alicdn.com/imgextra/i4/1789418796/TB20Kt4hFXXXXbKXpXXXXXXXXXX_!!1789418796.jpg' , 'seller_id':'1789418796' , 'activity_id':'2dd1ec9c8ee14264bfccc867eb8612b2'}
 ];
