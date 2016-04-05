1beeea6d6e99916c575800e050452ebd931a8473
yunbin.wangyb <yunbin.wangyb@alibaba-inc.com>
2016-04-05 09:55:54 +0800

    提交

--- js/data.js
+++ js/data.js
@@ -2,5 +2,6 @@
  * Created by admin on 16/1/30.
  */
 var datas = [
-    {'name':'Foot宝宝优惠券','valid':'2016.04.12前有效','discount':'10','condition':'满 50 使用','img':'https://img.alicdn.com/imgextra/i4/1789418796/TB20Kt4hFXXXXbKXpXXXXXXXXXX_!!1789418796.jpg' , 'seller_id':'1789418796' , 'activity_id':'dc68910223d140b0bd3567779d8219c0'}
+    {'name':'Foot宝宝优惠券','valid':'2016.04.12前有效','discount':'10','condition':'满 50 使用','img':'https://img.alicdn.com/imgextra/i4/1789418796/TB20Kt4hFXXXXbKXpXXXXXXXXXX_!!1789418796.jpg' , 'seller_id':'1789418796' , 'activity_id':'dc68910223d140b0bd3567779d8219c0'},
+    {'name':'Foot宝宝优惠券','valid':'2016.04.12前有效','discount':'20','condition':'满 50 使用','img':'https://img.alicdn.com/imgextra/i4/1789418796/TB20Kt4hFXXXXbKXpXXXXXXXXXX_!!1789418796.jpg' , 'seller_id':'1789418796' , 'activity_id':'cc54c6a5994b4f16ae47a709561533e2'}
 ];
